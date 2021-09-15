54480e85f480cbb5ae2b71b74ed28f1681c9b5f1
bl <mail@benedikt.tech>
2021-09-15 07:57:40 +0200

    Improved UI

--- hashcatGUI/frm_settings.Designer.cs
+++ hashcatGUI/frm_settings.Designer.cs
@@ -37,10 +37,11 @@ namespace hashcatGUI
             this.rb_bruteforce = new System.Windows.Forms.RadioButton();
             this.rb_wordlist = new System.Windows.Forms.RadioButton();
             this.gb_useddevice = new System.Windows.Forms.GroupBox();
-            this.rb_apu = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_desCpu = new System.Windows.Forms.Label();
             this.rb_gpu = new System.Windows.Forms.RadioButton();
             this.rb_cpu = new System.Windows.Forms.RadioButton();
-            this.rb_straight = new System.Windows.Forms.RadioButton();
+            this.btn_apply = new System.Windows.Forms.Button();
             this.pn_top.SuspendLayout();
             this.gb_attackmode.SuspendLayout();
             this.gb_useddevice.SuspendLayout();
@@ -48,6 +49,7 @@ namespace hashcatGUI
             // 
             // btn_minimize
             // 
+            this.btn_minimize.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_minimize.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_minimize.Location = new System.Drawing.Point(561, 9);
             this.btn_minimize.Name = "btn_minimize";
@@ -59,6 +61,7 @@ namespace hashcatGUI
             // 
             // btn_close
             // 
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_close.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_close.Location = new System.Drawing.Point(658, 9);
             this.btn_close.Name = "btn_close";
@@ -75,18 +78,17 @@ namespace hashcatGUI
             this.pn_top.Dock = System.Windows.Forms.DockStyle.Top;
             this.pn_top.Location = new System.Drawing.Point(0, 0);
             this.pn_top.Name = "pn_top";
-            this.pn_top.Size = new System.Drawing.Size(760, 47);
+            this.pn_top.Size = new System.Drawing.Size(762, 47);
             this.pn_top.TabIndex = 12;
             // 
             // gb_attackmode
             // 
-            this.gb_attackmode.Controls.Add(this.rb_straight);
             this.gb_attackmode.Controls.Add(this.rb_bruteforce);
             this.gb_attackmode.Controls.Add(this.rb_wordlist);
             this.gb_attackmode.Font = new System.Drawing.Font("Source Code Pro", 18F);
             this.gb_attackmode.Location = new System.Drawing.Point(49, 64);
             this.gb_attackmode.Name = "gb_attackmode";
-            this.gb_attackmode.Size = new System.Drawing.Size(292, 416);
+            this.gb_attackmode.Size = new System.Drawing.Size(292, 262);
             this.gb_attackmode.TabIndex = 16;
             this.gb_attackmode.TabStop = false;
             this.gb_attackmode.Text = "Attack Mode";
@@ -95,50 +97,61 @@ namespace hashcatGUI
             // 
             this.rb_bruteforce.AutoSize = true;
             this.rb_bruteforce.Font = new System.Drawing.Font("Source Code Pro", 18F);
-            this.rb_bruteforce.Location = new System.Drawing.Point(35, 291);
+            this.rb_bruteforce.Location = new System.Drawing.Point(35, 187);
             this.rb_bruteforce.Name = "rb_bruteforce";
             this.rb_bruteforce.Size = new System.Drawing.Size(186, 35);
             this.rb_bruteforce.TabIndex = 15;
-            this.rb_bruteforce.TabStop = true;
             this.rb_bruteforce.Text = "Brute Force";
             this.rb_bruteforce.UseVisualStyleBackColor = true;
+            this.rb_bruteforce.CheckedChanged += new System.EventHandler(this.attackMode_Changed);
             // 
             // rb_wordlist
             // 
             this.rb_wordlist.AutoSize = true;
+            this.rb_wordlist.Checked = true;
             this.rb_wordlist.Font = new System.Drawing.Font("Source Code Pro", 18F);
-            this.rb_wordlist.Location = new System.Drawing.Point(35, 163);
+            this.rb_wordlist.Location = new System.Drawing.Point(35, 59);
             this.rb_wordlist.Name = "rb_wordlist";
             this.rb_wordlist.Size = new System.Drawing.Size(158, 35);
             this.rb_wordlist.TabIndex = 14;
             this.rb_wordlist.TabStop = true;
             this.rb_wordlist.Text = "Word List";
             this.rb_wordlist.UseVisualStyleBackColor = true;
+            this.rb_wordlist.CheckedChanged += new System.EventHandler(this.attackMode_Changed);
             // 
             // gb_useddevice
             // 
-            this.gb_useddevice.Controls.Add(this.rb_apu);
+            this.gb_useddevice.Controls.Add(this.label1);
+            this.gb_useddevice.Controls.Add(this.lbl_desCpu);
             this.gb_useddevice.Controls.Add(this.rb_gpu);
             this.gb_useddevice.Controls.Add(this.rb_cpu);
             this.gb_useddevice.Font = new System.Drawing.Font("Source Code Pro", 18F);
             this.gb_useddevice.Location = new System.Drawing.Point(414, 64);
             this.gb_useddevice.Name = "gb_useddevice";
-            this.gb_useddevice.Size = new System.Drawing.Size(292, 416);
+            this.gb_useddevice.Size = new System.Drawing.Size(292, 262);
             this.gb_useddevice.TabIndex = 17;
             this.gb_useddevice.TabStop = false;
             this.gb_useddevice.Text = "Used Device";
             // 
-            // rb_apu
+            // label1
             // 
-            this.rb_apu.AutoSize = true;
-            this.rb_apu.Font = new System.Drawing.Font("Source Code Pro", 18F);
-            this.rb_apu.Location = new System.Drawing.Point(35, 291);
-            this.rb_apu.Name = "rb_apu";
-            this.rb_apu.Size = new System.Drawing.Size(74, 35);
-            this.rb_apu.TabIndex = 15;
-            this.rb_apu.TabStop = true;
-            this.rb_apu.Text = "APU";
-            this.rb_apu.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Recommended for HIGH profile PCs";
+            // 
+            // lbl_desCpu
+            // 
+            this.lbl_desCpu.AutoSize = true;
+            this.lbl_desCpu.Font = new System.Drawing.Font("Source Code Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_desCpu.Location = new System.Drawing.Point(18, 89);
+            this.lbl_desCpu.Name = "lbl_desCpu";
+            this.lbl_desCpu.Size = new System.Drawing.Size(256, 17);
+            this.lbl_desCpu.TabIndex = 15;
+            this.lbl_desCpu.Text = "Recommended for LOW profile PCs";
             // 
             // rb_gpu
             // 
@@ -148,13 +161,14 @@ namespace hashcatGUI
             this.rb_gpu.Name = "rb_gpu";
             this.rb_gpu.Size = new System.Drawing.Size(74, 35);
             this.rb_gpu.TabIndex = 14;
-            this.rb_gpu.TabStop = true;
             this.rb_gpu.Text = "GPU";
             this.rb_gpu.UseVisualStyleBackColor = true;
+            this.rb_gpu.CheckedChanged += new System.EventHandler(this.usedDevice_Changed);
             // 
             // rb_cpu
             // 
             this.rb_cpu.AutoSize = true;
+            this.rb_cpu.Checked = true;
             this.rb_cpu.Font = new System.Drawing.Font("Source Code Pro", 18F);
             this.rb_cpu.Location = new System.Drawing.Point(35, 51);
             this.rb_cpu.Name = "rb_cpu";
@@ -163,24 +177,26 @@ namespace hashcatGUI
             this.rb_cpu.TabStop = true;
             this.rb_cpu.Text = "CPU";
             this.rb_cpu.UseVisualStyleBackColor = true;
+            this.rb_cpu.CheckedChanged += new System.EventHandler(this.usedDevice_Changed);
             // 
-            // rb_straight
+            // btn_apply
             // 
-            this.rb_straight.AutoSize = true;
-            this.rb_straight.Font = new System.Drawing.Font("Source Code Pro", 18F);
-            this.rb_straight.Location = new System.Drawing.Point(35, 51);
-            this.rb_straight.Name = "rb_straight";
-            this.rb_straight.Size = new System.Drawing.Size(144, 35);
-            this.rb_straight.TabIndex = 16;
-            this.rb_straight.TabStop = true;
-            this.rb_straight.Text = "Straight";
-            this.rb_straight.UseVisualStyleBackColor = true;
+            this.btn_apply.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_apply.Font = new System.Drawing.Font("Source Code Pro", 18F, System.Drawing.FontStyle.Bold);
+            this.btn_apply.Location = new System.Drawing.Point(303, 367);
+            this.btn_apply.Name = "btn_apply";
+            this.btn_apply.Size = new System.Drawing.Size(159, 44);
+            this.btn_apply.TabIndex = 18;
+            this.btn_apply.Text = "Apply";
+            this.btn_apply.UseVisualStyleBackColor = true;
+            this.btn_apply.Click += new System.EventHandler(this.btn_apply_Click);
             // 
             // frm_settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(760, 507);
+            this.ClientSize = new System.Drawing.Size(762, 448);
+            this.Controls.Add(this.btn_apply);
             this.Controls.Add(this.gb_useddevice);
             this.Controls.Add(this.pn_top);
             this.Controls.Add(this.gb_attackmode);
@@ -205,9 +221,10 @@ namespace hashcatGUI
         private System.Windows.Forms.RadioButton rb_bruteforce;
         private System.Windows.Forms.RadioButton rb_wordlist;
         private System.Windows.Forms.GroupBox gb_useddevice;
-        private System.Windows.Forms.RadioButton rb_apu;
         private System.Windows.Forms.RadioButton rb_gpu;
         private System.Windows.Forms.RadioButton rb_cpu;
-        private System.Windows.Forms.RadioButton rb_straight;
+        private System.Windows.Forms.Button btn_apply;
+        private System.Windows.Forms.Label lbl_desCpu;
+        private System.Windows.Forms.Label label1;
     }
 }
--- hashcatGUI/frm_settings.cs
+++ hashcatGUI/frm_settings.cs
@@ -12,6 +12,11 @@ namespace hashcatGUI
 {
     public partial class frm_settings : Form
     {
+
+        public byte? AttackMode;
+        public byte? UsedDevice;
+        public DialogResult result;
+
         public frm_settings()
         {
             InitializeComponent();
@@ -35,20 +40,20 @@ namespace hashcatGUI
             btn_minimize.FlatStyle = FlatStyle.Flat;
             btn_minimize.FlatAppearance.BorderSize = 1;
 
+            // btn_apply Style
+            btn_apply.ForeColor = Color.FromArgb(72, 127, 72);
+            btn_apply.BackColor = Color.FromArgb(0, 0, 0);
+            btn_apply.FlatStyle = FlatStyle.Flat;
+            btn_apply.FlatAppearance.BorderSize = 1;
+
             // rb_bruteforce Style
             rb_bruteforce.ForeColor = Color.FromArgb(72, 127, 72);
 
             // rb_gpu Style
             rb_gpu.ForeColor = Color.FromArgb(72, 127, 72);
 
-            // rb_apu Style
-            rb_apu.ForeColor = Color.FromArgb(72, 127, 72);
-
             // rb_cpu Style
             rb_cpu.ForeColor = Color.FromArgb(72, 127, 72);
-
-            // rb_straight Style
-            rb_straight.ForeColor = Color.FromArgb(72, 127, 72);
 
             // rb_wordlist Style
             rb_wordlist.ForeColor = Color.FromArgb(72, 127, 72);
@@ -70,6 +75,52 @@ namespace hashcatGUI
         private void btn_minimize_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_apply_Click(object sender, EventArgs e)
+        {
+            if (this.AttackMode == null | this.UsedDevice == null)
+            {
+                this.AttackMode = 0;
+                this.UsedDevice = 1;
+            }
+            this.result = DialogResult.OK;
+            this.Close();
+
+        }
+
+        private void attackMode_Changed(Object sender, EventArgs e)
+        {
+           if(((RadioButton)sender).Checked)
+            {
+                RadioButton rb = (RadioButton)sender;
+                switch (rb.Name)
+                {
+                    case "rb_bruteforce":
+                        this.AttackMode = 3;
+                        break;
+                    case "rb_wordlist":
+                        this.AttackMode = 0;
+                        break;
+                }
+            }
+        }
+
+        private void usedDevice_Changed(Object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                RadioButton rb = (RadioButton)sender;
+                switch (rb.Name)
+                {
+                    case "rb_cpu":
+                        this.UsedDevice = 1;
+                        break;
+                    case "rb_gpu": 
+                        this.UsedDevice = 2;
+                        break;
+                }
+            }
         }
     }
 }
